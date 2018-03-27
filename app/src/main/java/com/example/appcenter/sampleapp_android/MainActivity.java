afeefe0dc253ed1a61872fa7f95ce326ba4f869f
shrubby <cjc00@hotmail.com>
2018-03-27 09:24:36 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/appcenter/sampleapp_android/MainActivity.java
+++ app/src/main/java/com/example/appcenter/sampleapp_android/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppCenter.start(getApplication(), "<APP SECRET HERE>",
+        AppCenter.start(getApplication(), "3e4d3f28-550e-43ef-b61f-67fe96b25c5a",
                 Analytics.class, Crashes.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
